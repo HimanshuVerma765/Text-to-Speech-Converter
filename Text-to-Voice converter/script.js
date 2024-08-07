48d9af3f53a5165e07022ab444b36123daba642b
Himanshu Verma <166311209+HimanshuVerma765@users.noreply.github.com>
2024-08-07 16:20:50 +0530

    Add files via upload
    all files are added now it's your turn to make it better

---
+++ Text-to-Voice converter/script.js
@@ -0,0 +1,17 @@
+let speech = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[2];
+
+    voices.forEach((voice,i) => (voiceSelect.options[i] = new options(voice.name, i)));
+};
+
+document.querySelector("button").addEventListener("click",() =>{
+    speech.text=document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+})
